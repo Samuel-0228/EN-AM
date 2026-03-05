09568966ffdbb803aabfae36cf779f075b459a7d
samuelytt <samuaelyeshambel@gmail.com>
2026-03-05 21:55:12 +0300

    update

--- translator.py
+++ translator.py
@@ -6,6 +6,7 @@ from deep_translator import GoogleTranslator
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 
+
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN is not set")
 
